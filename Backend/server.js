b34620eb04bfae2a228b528a76f2ad9e1dd87763
aasthachauhan004 <154447310+aasthachauhan004@users.noreply.github.com>
2025-03-17 23:10:18 +0530

    added-delete-product-func

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,17 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
-console.log(process.env.MONGO_URI);
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
